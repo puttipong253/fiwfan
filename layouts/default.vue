bc48129df5c32b44efdff58cdf0ced743d8c8980
phutthiphong <james.jirawuttinan@gmail.com>
2021-08-05 13:13:16 +0700

    responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,34 @@
 <template>
     <div>
         <div class='bg-pink-1'>
-            <nav class='container mx-auto px-16 py-5 flex justify-between'>
+            <nav class='container mx-auto px-4 xl:px-[6rem] py-3 lg:py-5 flex justify-between items-center'>
                 <div class='font-bold text-2xl text-pink-2 cursor-pointer'>
                     LOGO
                 </div>
 
-                <div class='flex items-center'>
-                    <div class='font-medium text-base text-pink-2 mx-6 cursor-pointer'>
-                        กิจกรรม
-                    </div>
-                    <div class='font-medium text-base text-pink-2 mx-6 cursor-pointer'>
-                        กระดานผู้นำท่องเที่ยว
-                    </div>
-                    <div class='font-medium text-base text-pink-2 mx-6 cursor-pointer'>
-                        การบ้านคุณภาพ
-                    </div>
-                    <div class='font-medium text-base text-pink-2 mx-6 cursor-pointer'>
-                        พูดคุยการบ้าน/ติดต่อแอดมิน
+                <div class='hidden md:block'>
+                    <div class='flex items-center font-medium text-sm lg:text-base text-pink-2'>
+                        <div class='mx-2 xl:mx-6 cursor-pointer'>
+                            กิจกรรม
+                        </div>
+                        <div class='mx-2 xl:mx-6 cursor-pointer'>
+                            กระดานผู้นำท่องเที่ยว
+                        </div>
+                        <div class='mx-2 xl:mx-6 cursor-pointer'>
+                            การบ้านคุณภาพ
+                        </div>
+                        <div class='mx-2 xl:mx-6 cursor-pointer'>
+                            พูดคุยการบ้าน/ติดต่อแอดมิน
+                        </div>
                     </div>
                 </div>
 
                 <div>
-                    <button class='font-medium text-base text-black rounded-full px-4 py-1 mr-5'>
+                    <button class='font-medium text-sm lg:text-base text-black rounded-full px-4 py-1 lg:mr-5'>
                         Sign In
                     </button>
 
-                    <button class='font-normal text-base bg-pink-2 text-white rounded-full px-4 py-1'>
+                    <button class='font-normal text-sm lg:text-base bg-pink-2 text-white rounded-full px-4 py-1'>
                         Sign Up
                     </button>
                 </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,84 +1,105 @@
 <template>
     <div>
         <div class='bg-pink-1'>
-            <div class='flex justify-center relative'>
-                <img src='../assets/image/Frame.png' alt='Frame' width='55%' />
-                <div class='absolute mt-[6.5rem] w-[41rem]'>
-                    <div class='text-[55px] font-semibold text-center text-pink-2 mb-7'>
-                        เปิดประสบการณ์ใหม่กับเรา
-                    </div>
-                    <div class='text-[25px] text-center text-pink-2 mb-3'>
-                        เว็บไซต์รวบรวมน้อง ๆ ไซด์ไลน์ สไตล์ฟิวแฟนทะเลเรียกว่า
-                    </div>
-                    <div class='text-[25px] text-center text-pink-2'>
-                        Sea ช่วงเวลาดี ๆ เรียกว่า Fiwfans
-                    </div>
-                    <div
-                        class='shadow-pink bg-white mt-16 py-2 pr-2 pl-6 rounded-full bg-opacity-[0.7] shadow-md flex justify-between items-center'>
-                        <div class='border-r border-[#D85CB5] pr-5'>
-                            <div class='flex items-center'>
-                                <div class='text-pink-2 mr-1 text-sm'>
-                                    คุณกำลังมองหา
-                                </div>
-                                <svg-icon
-                                    name='eva_arrow-ios-downward-fill'
-                                    width='20'
-                                    height='20'
-                                    class='text-pink-2'
-                                />
-                            </div>
-                            <div class='mt-2 text-sm'>
-                                ผู้หญิง
-                            </div>
+            <div class='container mx-auto'>
+                <div class='md:hidden sm:flex sm:justify-center'>
+                    <div class='flex items-center text-xs justify-center'>
+                        <div class='font-medium text-pink-2 cursor-pointer mr-4'>
+                            กิจกรรม
                         </div>
-                        <div class='border-r border-[#D85CB5] pr-5'>
-                            <div class='flex items-center'>
-                                <div class='text-pink-2 mr-1 text-sm'>
-                                    ช่วงอายุที่คุณต้องการค้นหา
-                                </div>
-                                <svg-icon
-                                    name='eva_arrow-ios-downward-fill'
-                                    width='20'
-                                    height='20'
-                                    class='text-pink-2'
-                                />
-                            </div>
-                            <div class='mt-2 text-sm'>
-                                19 - 25
-                            </div>
+                        <div class='font-medium text-pink-2 cursor-pointer'>
+                            กระดานผู้นำท่องเที่ยว
                         </div>
-                        <div class=''>
-                            <div class='flex items-center'>
-                                <div class='text-pink-2 mr-1 text-sm'>
-                                    จังหวัดที่ต้องการค้นหา
-                                </div>
-                                <svg-icon
-                                    name='eva_arrow-ios-downward-fill'
-                                    width='20'
-                                    height='20'
-                                    class='text-pink-2'
-                                />
-                            </div>
-                            <div class='mt-2 text-sm text-[#CBCBCB]'>
-                                เลือกจังหวัดที่ต้องการ
-                            </div>
+                    </div>
+                    <div class='flex items-center text-xs justify-center border-b mx-8 pb-4 border-pink-2'>
+                        <div class='font-medium text-pink-2 cursor-pointer mr-4'>
+                            การบ้านคุณภาพ
                         </div>
-                        <div>
-                            <svg-icon
-                                name='foundation_magnifying-glass'
-                                width='50'
-                                height='50'
-                                class='bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] rounded-full px-1 text-white'
-                            />
+                        <div class='font-medium text-pink-2 cursor-pointer'>
+                            พูดคุยการบ้าน/ติดต่อแอดมิน
+                        </div>
+                    </div>
+                </div>
+
+                <div class='flex justify-center relative w-full'>
+                    <img src='../assets/image/Frame.png' alt='Frame' width='55%' />
+                    <div class='absolute mt-6 xl:mt-[6.5rem] px-3 lg:px-[20rem]'>
+                        <div class='text-xl xl:text-[55px] font-semibold text-center text-pink-2 mb-2 lg:mb-8'>
+                            เปิดประสบการณ์ใหม่กับเรา
+                        </div>
+                        <div class='text-sm xl:text-[25px] text-pink-2 mb-3 text-center lg:leading-10'>
+                            เว็บไซต์รวบรวมน้อง ๆ ไซด์ไลน์ สไตล์ฟิวแฟน ทะเลเรียกว่า Sea ช่วงเวลาดี ๆ เรียกว่า Fiwfans
+                        </div>
+                        <div class='hidden lg:block '>
+                            <div
+                                class='shadow-pink bg-white mt-5 xl:mt-16 py-2 pr-2 pl-6 rounded-full bg-opacity-[0.7] shadow-md flex justify-between items-center'>
+                                <div class='border-r border-[#D85CB5] pr-5'>
+                                    <div class='flex items-center'>
+                                        <div class='text-pink-2 mr-1 text-sm'>
+                                            คุณกำลังมองหา
+                                        </div>
+                                        <svg-icon
+                                            name='eva_arrow-ios-downward-fill'
+                                            width='20'
+                                            height='20'
+                                            class='text-pink-2'
+                                        />
+                                    </div>
+                                    <div class='mt-2 text-sm'>
+                                        ผู้หญิง
+                                    </div>
+                                </div>
+                                <div class='border-r border-[#D85CB5] pr-5'>
+                                    <div class='flex items-center'>
+                                        <div class='text-pink-2 mr-1 text-sm'>
+                                            ช่วงอายุที่คุณต้องการค้นหา
+                                        </div>
+                                        <svg-icon
+                                            name='eva_arrow-ios-downward-fill'
+                                            width='20'
+                                            height='20'
+                                            class='text-pink-2'
+                                        />
+                                    </div>
+                                    <div class='mt-2 text-sm'>
+                                        19 - 25
+                                    </div>
+                                </div>
+                                <div class=''>
+                                    <div class='flex items-center'>
+                                        <div class='text-pink-2 mr-1 text-sm'>
+                                            จังหวัดที่ต้องการค้นหา
+                                        </div>
+                                        <svg-icon
+                                            name='eva_arrow-ios-downward-fill'
+                                            width='20'
+                                            height='20'
+                                            class='text-pink-2'
+                                        />
+                                    </div>
+                                    <div class='mt-2 text-sm text-[#CBCBCB]'>
+                                        เลือกจังหวัดที่ต้องการ
+                                    </div>
+                                </div>
+                                <div>
+                                    <svg-icon
+                                        name='foundation_magnifying-glass'
+                                        width='50'
+                                        height='50'
+                                        class='bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] rounded-full px-1 text-white'
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
 
-        <div class='bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] h-[29rem]'>
-            <div class='container mx-auto py-7'>
-                <div class='flex justify-between px-[6rem] text-white mb-5'>
+        <div class='bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] h-[25rem] xl:h-[29rem]'>
+            <div class='container mx-auto py-7 w-full'>
+                <div class='flex justify-between px-5 xl:px-[6rem] text-white xl:mb-5'>
                     <div class='flex'>
                         <svg-icon
                             name='bi_bookmark-star-fill'
@@ -93,29 +114,39 @@
                     </div>
                 </div>
 
-                <swiper ref='swiper' :options='swiperOption'>
+                <swiper ref='swiper' :options='swiperOption' class='header'>
                     <swiper-slide>
-                        <div class='px-[6rem] py-2'>
-                            <div class='grid grid-cols-6 gap-[2.5rem]'>
-                                <div v-for='(item,index) in affiliation' :key='index'
-                                     class='bg-white w-[100px] lg:w-[150px] h-[150px] py-3 px-5 rounded-xl w-full hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
+                        <div class='xl:px-[6rem] px-10 py-5'>
+                            <div class='grid grid-cols-3 lg:grid-cols-6 gap-2 xl:gap-[2.5rem]'>
+                                <div v-for='(item,index) in guildThree()' :key='index'
+                                     class='lg:hidden bg-white w-[90px] h-[90px] lg:w-[150px] lg:h-[150px] py-3 px-5 rounded-xl w-full hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
                                     <div class='flex justify-center'>
                                         <img :src='item.image' alt='' />
                                     </div>
-                                    <span class='text-[#33333] flex justify-center mt-1'>{{ item.name }}</span>
+                                    <span class='text-[#33333] text-xs lg:text-sm flex justify-center mt-1'>{{ item.name
+                                        }}</span>
+                                </div>
+                                <div v-for='(item,index) in guildSix()' :key='index'
+                                     class='hidden lg:block bg-white w-[90px] h-[90px] lg:w-[150px] lg:h-[150px] py-3 px-5 rounded-xl w-full hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
+                                    <div class='flex justify-center'>
+                                        <img :src='item.image' alt='' />
+                                    </div>
+                                    <span class='text-[#33333] text-xs lg:text-sm flex justify-center mt-1'>{{ item.name
+                                        }}</span>
                                 </div>
                             </div>
                         </div>
                     </swiper-slide>
                     <swiper-slide>
-                        <div class='px-[6rem] py-2'>
-                            <div class='grid grid-cols-6 gap-[2.5rem]'>
-                                <div v-for='(item,index) in affiliation' :key='index'
-                                     class='bg-white w-[100px] lg:w-[150px] h-[150px] py-3 px-5 rounded-xl w-full hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
+                        <div class='xl:px-[6rem] px-10 py-5'>
+                            <div class='grid grid-cols-3 lg:grid-cols-6 gap-2 xl:gap-[2.5rem]'>
+                                <div v-for='(item,index) in guildThree()' :key='index'
+                                     class='bg-white w-[90px] h-[90px] lg:w-[150px] lg:h-[150px] py-3 px-5 rounded-xl w-full hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
                                     <div class='flex justify-center'>
                                         <img :src='item.image' alt='' />
                                     </div>
-                                    <span class='text-[#33333] flex justify-center mt-1'>{{ item.name }}</span>
+                                    <span class='text-[#33333] text-xs lg:text-sm flex justify-center mt-1'>{{ item.name
+                                        }}</span>
                                 </div>
                             </div>
                         </div>
@@ -126,9 +157,9 @@
             </div>
         </div>
 
-        <div class='container mx-auto py-7 px-[6rem] mb-5'>
-            <div class='bg-pink-1 text-center py-6'>
-                <div class='px-[14rem]'>
+        <div class='container mx-auto py-7 px-10 xl:px-[6rem] mb-5'>
+            <div class='bg-pink-1 text-center py-6 w-full'>
+                <div class='xl:px-[14rem] w-full'>
                     <div class='text-lg text-pink-2 mb-3 font-medium'>Fiwfan.com สำหรับผู้ที่มีอายุ 20
                         ปีขึ้นไปเท่านั้น
                     </div>
@@ -139,7 +170,7 @@
                     </div>
                 </div>
             </div>
-            <div class='mt-5'>
+            <div class='mt-5 w-full'>
                 <div
                     class='w-full flex items-center justify-between bg-gradient-to-b from-pink-3 to-[#FAB4CE] py-3 rounded-md px-3 text-white'>
                     <div class='flex'>
@@ -150,7 +181,7 @@
                         />
                         <span class='ml-2'>รายการทั้งหมด 535 รายการ</span>
                     </div>
-                    <div class='relative'>
+                    <div class='relative w-full'>
                         <input
                             class='w-[15rem] text-sm rounded-md px-3 py-2 bg-dark-1 focus:outline-none font-light text-pink-2 placeholder-pink-2'
                             placeholder='ให้เราช่วยค้นหา...' />
@@ -163,7 +194,7 @@
                     </div>
                 </div>
             </div>
-            <div class='mt-4 grid grid-cols-4 gap-[1.25rem]'>
+            <div class='mt-4 grid grid-cols-1 lg:grid-cols-4 gap-[1.25rem]'>
                 <div v-for='(item,index) in userData' :key='index'>
                     <div
                         class='relative w-[16rem] h-[22.5rem] bg-white shadow-md rounded-md cursor-pointer hover:shadow-xl transform transition duration-150 ease-in-out hover:-translate-y-2'>
@@ -190,7 +221,7 @@
                         </div>
                         <div class='absolute bottom-[8rem] right-0 px-3'>
                             <div v-show='item.status'
-                                class='px-4 py-1 text-xs text-white bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] rounded opacity-[0.8]'>
+                                 class='px-4 py-1 text-xs text-white bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] rounded opacity-[0.8]'>
                                 ยืนยันตัวตนแล้ว
                             </div>
                         </div>
@@ -198,24 +229,24 @@
                             <div class='flex justify-between items-center mb-2'>
                                 <div
                                     class='px-4 text-xs text-white bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] rounded'>
-                                    {{item.province}}
+                                    {{ item.province }}
                                 </div>
                                 <div class='text-xs text-pink-4'>
-                                    {{item.district}}
+                                    {{ item.district }}
                                 </div>
                             </div>
                             <div class='text-sm text-pink-4 font-medium mb-1'>
-                                {{item.name}}
+                                {{ item.name }}
                             </div>
                             <div class='text-sm text-gray-2'>
-                                {{item.sex}}
+                                {{ item.sex }}
                             </div>
                             <div class='flex justify-between items-center'>
                                 <div class='text-sm text-gray-2'>
-                                    อายุ {{item.age}}
+                                    อายุ {{ item.age }}
                                 </div>
                                 <div class='text-2xl text-pink-4 font-semibold'>
-                                    ฿ {{addCommas(item.price)}}
+                                    ฿ {{ addCommas(item.price) }}
                                 </div>
                             </div>
                         </div>
@@ -223,7 +254,6 @@
                 </div>
             </div>
         </div>
-
         <div class='bg-gradient-to-b from-[#D554A9] to-[#FAB4CE] h-14'>
 
         </div>
@@ -408,7 +438,7 @@ export default {
                     province: 'เชียงใหม่',
                     district: 'หนองป่าครั่ง',
                     status: true
-                },
+                }
             ],
 
             affiliation: [
@@ -463,12 +493,29 @@ export default {
             ]
         }
     },
+
+    computed: {},
+
     methods: {
         prev() {
             this.$refs.swiper.$swiper.slidePrev()
         },
         next() {
             this.$refs.swiper.$swiper.slideNext()
+        },
+        guildThree() {
+            const data = []
+            for (let i = 0; i < 9; i++) {
+                data.push(this.affiliation[i])
+            }
+            return data
+        },
+        guildSix() {
+            const data = []
+            for (let i = 0; i < 12; i++) {
+                data.push(this.affiliation[i])
+            }
+            return data
         }
     }
 }
@@ -480,6 +527,6 @@ export default {
 }
 
 .header {
-    //padding: 9rem 0px;
+    padding: 0rem 0px;
 }
 </style>
